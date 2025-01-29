acd7be2fd4bd2118d315c47ceacc6c2a07cadca5
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2025-01-29 12:32:42 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+  default     = "eks-cluster"
+}
+
+variable "region" {
+  description = "The name of the EKS cluster."
+  type        = string
+  default     = "us-east-1"
+}
